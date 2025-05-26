56f8cb4b73f739c2826a9318cbba2acbe029d278
laisaschonss <laisaschonss2@gmail.com>
2025-05-26 10:58:46 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function criaCartao(categoria,pergunta,resposta){
+  let container= document.getElementByld('container)
+  let cartao= document.createElement(article')
+  cartao.className= 'cartao'
+
+    cartao.innerHTML=
+  <div class="cartao_conteudo">
+  <h3>${categoria}<\h3>
+  <
